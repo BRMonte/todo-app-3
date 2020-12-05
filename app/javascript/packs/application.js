ae7a0785a6ae501360bad1afdedf48d50c29c700
Bruno Montebello <brunormontebello@hotmail.com>
2020-12-05 20:49:06 +0000

    Config Bootsrap + React

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+require('bootstrap')
+import 'bootstrap/dist/css/bootstrap'
+require('./hello_react')
