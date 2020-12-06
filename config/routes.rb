361aeee08e4b363e6a6f69bb8f0e5b67f74c2e9b
Bruno Montebello <brunormontebello@hotmail.com>
2020-12-06 20:01:30 +0000

    Create3 API: g todo_items controller + create nonauth endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'pages#home'
   authenticated :user do
     root "pages#my_todo_items", as: :authenticated_root
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :todo_items, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
 end
