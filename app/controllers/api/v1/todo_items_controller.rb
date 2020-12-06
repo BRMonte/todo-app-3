b42227e13f82da7cd1704b670513dc17e702e18b
Bruno Montebello <brunormontebello@hotmail.com>
2020-12-06 20:23:41 +0000

    Create3 API: create authorized endpoints

--- app/controllers/api/v1/todo_items_controller.rb
+++ app/controllers/api/v1/todo_items_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::TodoItemsController < ApplicationController
-  before_action :set_todo_item, only [:show, :edit, :update; :destroy]
-  def index
+  before_action :set_todo_item, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
+  def index
+    @todo_items = TodoItem.all
   end
 
   def show
@@ -24,5 +26,17 @@ class Api::V1::TodoItemsController < ApplicationController
 
   def set_todo_item
     @todo_item = TodoItem.find(params[:id])
+  end
+
+  def authorized?
+    @todo_item.user == current_user
+  end
+
+  def handle_unauthorized
+    unless authorized?
+      respond_to do |format|
+        format.json { render :unauthorized, status: 401 }
+      end
+    end
   end
 end
